998d7b5b2deb2ad423e87afbb1fe36b26b57085a
Valentin BORDEIANU <blenminer123@gmail.com>
2025-08-16 00:25:44 +0200

    Update Program.cs

--- PurrLay/Program.cs
+++ PurrLay/Program.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
  
 namespace PurrLay;
 
-internal static class Program      
+internal static class Program       
 {
     public static string SECRET_INTERNAL { get; private set; } = "PURRNET";
 
